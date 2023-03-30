81c0970ee2a1e058c7446e94b4b6755403a7e4d9
kwanu6774 <127473252+kwanu6774@users.noreply.github.com>
2023-03-30 15:54:52 +0900

    Add files via upload

---
+++ hw4.py
@@ -0,0 +1,15 @@
+def rep_char(c,n):
+    print(c*n)
+
+def welcome_msg(name):
+    msg1='Hello '+name
+    msg2='Welcome to Seoul'
+    nstr = len(msg1) if (len(msg1) > len(msg2)) else len(msg2)
+    rep_char('-',nstr+2)
+    print(f' {msg1},\n {msg2} ')
+    rep_char('-',nstr+2)
+
+name=input('Input his/her name:')
+print(name)
+welcome_msg(name)
+
